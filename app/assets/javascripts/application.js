7ba28ec76b3035efb1e6fe86937f171a5e66ad63
Thomas Sertorio <thomas.sertorio@gmail.com>
2017-09-21 20:49:12 +0200

    adjustment

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,9 +2,3 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
-
-(function() {
-  setInterval(function(){
-    $( "body" ).load( window.location.pathname + " .container")
-  }, 30000)
-}())
